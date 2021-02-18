4746429380c0ff78f7fbb90a3034888e783221c4
ctakayama <cole.takayama@gmail.com>
2021-02-18 00:43:12 -0800

    basic skeleton

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var path = require('path');
 var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
+var reader = require('./routes/reader');
+var confirm = require('./routes/confirm');
 // Example route
 // var user = require('./routes/user');
 
@@ -35,6 +37,8 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', index.view);
+app.get('/reader', reader.viewProject);
+app.get('/confirm', confirm.viewProject);
 // Example route
 // app.get('/users', user.list);
 
