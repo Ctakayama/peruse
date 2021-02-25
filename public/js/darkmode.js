67b9eb0e43904d5ba1ee63664a8ac1eb24542088
ctakayama <cole.takayama@gmail.com>
2021-02-25 14:20:10 -0800

    dark mode integrated

--- public/js/darkmode.js
+++ public/js/darkmode.js
@@ -1,20 +1,32 @@
-let toggle = true;
+let toggle = false;
 
 function test() {
+	console.log("running darkmode!");
+	var background = document.getElementsByClassName('ReadingText');
+	var thumbnail = document.getElementById("DarkThumb");
+	var label = document.getElementById("DarkLabel");
+	var el = document.getElementById("dark");
+	
+	el.classList.toggle("active");
+	console.log(label.innerHTML);
 
+	//light mode
 	if(toggle == true){
-		document.getElementById("light").className = "reader-nav LightMode unselectable";
-		document.body.style.backgroundColor = "black";
-		document.body.style.color = "white";
-		//document.getElementsByClassName('ReadingText').style.color = "white";
+		background[0].style.background = "#fff";
+		background[0].style.color = "#333";
+		thumbnail.src = "../images/iconmonstr-weather-111.svg";
 		//document.getElementsById('readText').style.color = "white";
+		label.innerHTML = "dark mode";
 		toggle = false;
+
+	//dark mode
 	} else {
-		document.getElementById("dark").className = "reader-nav DarkMode unselectable";
-		document.body.style.backgroundColor = "white";
-		document.body.style.color = "black";
-		//document.getElementsByClassName('ReadingText').style.color = "black";
+		
+		background[0].style.background = "#333";
+		background[0].style.color = "#eee";
 		//document.getElementsById('readText').style.color = "black";
+		thumbnail.src = "../images/iconmonstr-weather-2.svg";
+		label.innerHTML = "light mode";
 		toggle = true;
 	}
 }
