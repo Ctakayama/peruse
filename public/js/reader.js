e2765145ece8f4ab170dfdbc6ffcbd8ddd7b7f0e
ctakayama <cole.takayama@gmail.com>
2021-03-09 21:56:26 -0800

    mobile fix

--- public/js/reader.js
+++ public/js/reader.js
@@ -2,7 +2,7 @@
 var pages = [];
 var currentPg = 0;
 var totalPg = 0;
-var wpp = localStorage.getItem("wpp");
+
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
@@ -23,11 +23,11 @@ function initializePage() {
 
 function addContent(){
   var wc = getWordCount(localStorage.getItem("textContent"));
-  
-  console.log("words per page:"+ wpp);
+  // var wpp = localStorage.getItem("wpp");
+  // console.log("words per page:"+ wpp);
   console.log("this many words:" + wc);
   
-  splitIntoPages(localStorage.getItem("textContent"), wc, wpp);
+  splitIntoPages(localStorage.getItem("textContent"), wc, 250);
   let firstPg = pages[currentPg].join(' ');
   setHighlightSpeed(getWordCount(firstPg));
   $("#readText").html(firstPg);
